b3c324fc836e69a2b76560e98a93671bc6adc948
felixpasewaldt <felix.pasewaldt@campus.tu-berlin.de>
2025-04-05 12:56:32 +0200

    feat: add new TD3 error and step time CSV files, update experiment notebooks with additional episode summaries

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.experiments import halfchetah_experiment_sac, walker_experiment_sac,  walker_experiment_td3, halfcheetah_experiment_td3, ant_experiment_sac, quick_test_experiment
+from src.experiments import halfchetah_experiment_sac, walker_experiment_sac,  walker_experiment_td3, halfcheetah_experiment_td3, ant_experiment_sac, ant_experiment_td3
 from src.registration import register_all_envs
 
 register_all_envs()
 
 if __name__ == "__main__":
-    halfcheetah_experiment_td3({'mujoco': 0.75, 'brax': 0.25}, n_runs=1)
+    ant_experiment_td3({'mujoco': 0, 'brax': 1}, n_runs=1)
